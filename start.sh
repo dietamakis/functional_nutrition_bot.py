e7a7f4262d1dfe0bd8e6832a47c486543dd8546f
dietamakis <dieta.makis@gmail.com>
2025-05-21 20:16:32 +0500

    start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-echo "Starting fake server..." && sleep infinity &
-python functional_nutrition_bot.py
+
+# Запустить фальшивый HTTP-сервер на 10000 порту
+python3 -m http.server 10000 &
+
+# Запустить Telegram-бота
+python3 functional_nutrition_bot.py
